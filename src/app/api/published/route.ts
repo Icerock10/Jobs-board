6ca4ead401debd16d9fa061dbf649efa01a0df8b
Icerock10 <dp021190dms@gmail.com>
2023-09-11 14:31:03 +0300

    trying to update DB

--- src/app/api/published/route.ts
+++ src/app/api/published/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@/lib/db/connect-db';
 import Listing from '@/lib/db/models/Listing';
 
+export const revalidate = 0;
+
 export async function GET(req: NextRequest): Promise<NextResponse | unknown> {
   try {
     await connectDB();
