49ee4b1069d6d05cecbb098ac5d5194e61122e4f
Icerock10 <dp021190dms@gmail.com>
2023-09-18 20:37:11 +0300

    testing client component

--- src/_components/Header/Header.tsx
+++ src/_components/Header/Header.tsx
@@ -4,14 +4,16 @@ import { Menu } from '@/_components/Menu/Menu';
 import ThemeProvider from '@/_context/theme/ThemeProvider';
 import { Logo } from '@/_components/Logo/Logo';
 import { cookies } from 'next/headers';
+import { userService } from '@/_lib/services/api/user-service';
 
 export const Header = async () => {
   const token = cookies().get('token')?.value;
-  console.log(token);
+  const response = await userService.getAuthUser(token)
   return (
     <header className={styles.header}>
       <section className={styles.header__section}>
         <Logo />
+        {response?.data?.email}
         <aside className={styles.header_menu}>
           <ThemeProvider>
             <ThemeSwitcher email={token}/>
