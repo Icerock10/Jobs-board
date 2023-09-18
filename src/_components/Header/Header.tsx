c9110efb3c97d45f8a2d384378e8843ecfd036a2
Icerock10 <dp021190dms@gmail.com>
2023-09-18 20:08:05 +0300

    refactored middleware

--- src/_components/Header/Header.tsx
+++ src/_components/Header/Header.tsx
@@ -1,24 +1,18 @@
 import styles from './Header.module.scss';
 import { ThemeSwitcher } from '@/_components/ThemeSwitcher/ThemeSwitcher';
 import { Menu } from '@/_components/Menu/Menu';
-export const dynamic = 'force-dynamic';
 import ThemeProvider from '@/_context/theme/ThemeProvider';
 import { cookiesService } from '@/_lib/services/cookies/cookies-service';
 import { Logo } from '@/_components/Logo/Logo';
 import { userService } from '@/_lib/services/api/user-service';
-import { cookies } from 'next/headers';
 
 export const Header = async () => {
   const token = cookiesService.getToken()
   const response = await userService.getAuthUser(token);
-  console.log(response);
-  console.log(token);
-  console.log(cookies().get('token')?.value);
   return (
     <header className={styles.header}>
       <section className={styles.header__section}>
         <Logo />
-        Should be email{JSON.stringify(response?.data)}
         <aside className={styles.header_menu}>
           <ThemeProvider>
             <ThemeSwitcher email={response?.data?.email}/>
