edb3e25a3e1b3309465697b56c75a472cb7b8656
Icerock10 <dp021190dms@gmail.com>
2023-09-18 20:32:39 +0300

    testing client component

--- src/_components/Header/Header.tsx
+++ src/_components/Header/Header.tsx
@@ -3,17 +3,20 @@ import { ThemeSwitcher } from '@/_components/ThemeSwitcher/ThemeSwitcher';
 import { Menu } from '@/_components/Menu/Menu';
 import ThemeProvider from '@/_context/theme/ThemeProvider';
 import { Logo } from '@/_components/Logo/Logo';
+import { cookies } from 'next/headers';
 
 export const Header = async () => {
+  const token = cookies().get('token')?.value;
+  console.log(token);
   return (
     <header className={styles.header}>
       <section className={styles.header__section}>
         <Logo />
         <aside className={styles.header_menu}>
           <ThemeProvider>
-            <ThemeSwitcher email={'pd'}/>
+            <ThemeSwitcher email={token}/>
           </ThemeProvider>
-          <Menu email={'dp'}/>
+          <Menu email={token}/>
         </aside>
       </section>
     </header>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,6 @@ import { Inter } from 'next/font/google';
 import { Header } from '@/_components/Header/Header';
 import { ReduxProvider } from '@/store/ReduxProvider';
 import 'react-toastify/dist/ReactToastify.css';
-import { cookies } from 'next/headers';
-import { userService } from '@/_lib/services/api/user-service';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,16 +12,12 @@ export const metadata: Metadata = {
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  const token = cookies().get('token')?.value;
-  const response = token ? await userService.getAuthUser(token) : null
-  console.log(response);
   return (
     <html lang='en'>
     <body className={inter.className}>
     <ReduxProvider>
-      {response?.data?.email}
       <Header />
-      {children}
+      {/*{children}*/}
     </ReduxProvider>
     </body>
     </html>
